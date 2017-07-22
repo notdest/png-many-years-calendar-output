ddd012df2fcf4a791dd313a3f9a92bf915ef3792
Alexander Solodov <notdest@gmail.com>
2017-07-22 19:03:06 +0300

    Re-designed the printMonth()

--- generator.py
+++ generator.py
@@ -7,50 +7,52 @@ import datetime, calendar
 
 
 
-def printMonth(year,month,xst,yst,monWidth,draw):
-	monHeight 	= monWidth*0.85
+def printMonth(year,month,x,y,monthWidth,draw):
+	monthHeight 	= monthWidth*0.85
 
-	ceilw 	= round(monWidth/7) 										# ширина ячейки, под 7 дней недели
-	ceilh 	= round(monHeight/7)										# высота ячеки, 6 недель максимум + название месяца
+	cellWidth 		= round( monthWidth/7 )								# Cell width, for 7 days of the week
+	cellHeight 		= round( monthHeight/7 )							# Height of the cell, 6 weeks maximum + name of the month
 
-	#отладочные линии
+	# Lines for debugging the geometry of the month
 	if 0 :
-		draw.line((	xst, 			yst, 			xst+monWidth, 	yst				), fill="#000000", width=1)  #  --
-		draw.line((	xst, 			yst, 			xst, 			yst+monHeight	), fill="#000000", width=1)  #	|
-		draw.line((	xst, 			yst+monHeight, 	xst+monWidth, 	yst+monHeight	), fill="#000000", width=1)  #	_
-		draw.line((	xst+monWidth, 	yst, 			xst+monWidth, 	yst+monHeight	), fill="#000000", width=1)  #	  |
+		draw.line((	x, 				y, 				x+monthWidth, 	y				), fill="#000000", width=1)  #  --
+		draw.line((	x, 				y, 				x, 				y+monthHeight	), fill="#000000", width=1)  #	|
+		draw.line((	x, 				y+monthHeight, 	x+monthWidth, 	y+monthHeight	), fill="#000000", width=1)  #	_
+		draw.line((	x+monthWidth, 	y, 				x+monthWidth, 	y+monthHeight	), fill="#000000", width=1)  #	  |
 
 
-	# Написать название месяца
-	mnames 	= [u'Январь',u'Февраль',u'Март',u'Апрель',u'Май',u'Июнь',u'Июль',u'Август',u'Сентябрь',u'Октябрь',u'Ноябрь',u'Декабрь']
-	font 		= ImageFont.truetype("./fonts/Forum-Regular.otf", int(monWidth*0.075*1.5))
-	fsize = font.getsize(mnames[month-1])
-	draw.text((xst+(monWidth-fsize[0])/2, yst),mnames[month-1],(0,0,0),font=font) 
+	# Write the name of the month
+	names 		= [u'Январь',u'Февраль',u'Март',u'Апрель',u'Май',u'Июнь',u'Июль',u'Август',u'Сентябрь',u'Октябрь',u'Ноябрь',u'Декабрь']
+	# Names in English
+	# names 	= [u'January',u'February',u'March',u'April',u'May',u'June',u'July',u'August',u'September',u'October',u'November',u'December']
+	font 		= ImageFont.truetype("./fonts/Forum-Regular.otf", int(monthWidth*0.075*1.5))
+	nameSize 	= font.getsize(names[month-1])
+	draw.text(( x+(monthWidth-nameSize[0])/2 , y ), names[month-1], (0,0,0), font=font) 
 
-	# чуть подвинем циферки
-	xst += monWidth*0.03
+	# Figures should be moved slightly to the right
+	x += monthWidth*0.03
 
-	(wday,dcount)	= calendar.monthrange(year, month)
+	(dayOfTheWeek,daysCount)	= calendar.monthrange(year, month)
 
-	fontcifr 	= ImageFont.truetype("./fonts/BebasNeueLight.ttf", int(monWidth*0.075))
-	mlen 		= fontcifr.getsize(str(30))								# циферки будут по правому краю, это максимальная длина 
-	yst 		+= ceilh*0.4  											# чутка хочу сдвинуть название от чисел
+	fontNumeric = ImageFont.truetype("./fonts/BebasNeueLight.ttf", int(monthWidth*0.075))
+	maxLength 	= fontNumeric.getsize('30')								# Maximum length of the day of the month
+	y 			+= cellHeight*0.4  										# Posted numbers a little lower
 
-	i 		= 1
-	strnum 	= 1
-	while i<=dcount:
-		while wday<7 and i<=dcount:
-			rlen 	= fontcifr.getsize(str(i))
-			draw.text((xst+wday*ceilw+(mlen[0]-rlen[0])/2, yst+ceilh*strnum),str(i),(0,0,0),font=fontcifr )
+	i 			= 1
+	lineNum 	= 1
+	while i<=daysCount:
+		while dayOfTheWeek<7 and i<=daysCount:
+			length 	= fontNumeric.getsize(str(i))
+			draw.text(( x+ dayOfTheWeek*cellWidth + (maxLength[0]-length[0])/2 , y+ cellHeight*lineNum ),str(i),(0,0,0),font=fontNumeric )
 			i +=1
-			wday += 1;
+			dayOfTheWeek += 1;
 			pass
 
-		wday 	 = 0
-		strnum	+= 1
+		dayOfTheWeek 	 = 0
+		lineNum	+= 1
 
 	
-def printYear(year,byear,xst,yst,yearWidth,draw):
+def printYear(year,byear,x,y,yearWidth,draw):
 	stlb 		= 6	
 	ceilWidt 	= yearWidth/stlb
 	ceilHeight 	= yearWidth*0.166*0.8
@@ -67,15 +69,15 @@ def printYear(year,byear,xst,yst,yearWidth,draw):
 		capt += u' лет'
 
 	ysize 		= yfont.getsize(capt)
-	draw.text((xst+(yearWidth-ysize[0])/2, yst), capt ,(0,0,0),font=yfont)
-	yst 		+= ceilWidt*0.85*0.075*4
+	draw.text((x+(yearWidth-ysize[0])/2, y), capt ,(0,0,0),font=yfont)
+	y 		+= ceilWidt*0.85*0.075*4
 
 	# Выводим месяцы
 	i 		= 1
 	strn 	= 0
 	stlbn 	= 0
 	while i<13:
-		printMonth(year,i,xst+ceilWidt*stlbn,yst+strn*ceilHeight,ceilWidt*0.85,draw)
+		printMonth(year,i,x+ceilWidt*stlbn,y+strn*ceilHeight,ceilWidt*0.85,draw)
 
 		stlbn 	+= 1
 		if i%stlb == 0:
@@ -87,8 +89,8 @@ def printYear(year,byear,xst,yst,yearWidth,draw):
 def print60years(bYear,imageWidth,imageHeight,draw):
 	ceilWidt 	= imageWidth*0.2
 	ceilHeight 	= ceilWidt*0.295
-	xst 		= int(imageWidth*(20/float(paperWidth)))
-	yst 		= int(imageWidth*(10/float(paperWidth)))
+	x 		= int(imageWidth*(20/float(paperWidth)))
+	y 		= int(imageWidth*(10/float(paperWidth)))
 
 
 	ystr 	= 0
@@ -96,7 +98,7 @@ def print60years(bYear,imageWidth,imageHeight,draw):
 	i 		= 0
 
 	while i<60:
-		printYear(bYear+i,bYear,xst+ystlb*ceilWidt,yst+ystr*ceilHeight,ceilWidt*0.8,draw)
+		printYear(bYear+i,bYear,x+ystlb*ceilWidt,y+ystr*ceilHeight,ceilWidt*0.8,draw)
 		i 		+= 1
 		ystlb	+= 1
 		if ystlb > 4:
